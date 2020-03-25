097aa73358c4d7ebad825e736fc4037f9432ddae
Mykyta Chernenky <chernenm@uregina.ca>
2020-03-25 09:41:58 -0600

    mhm

---
+++ client/src/app/interfaces/customer.ts
@@ -0,0 +1,23 @@
+/*
+Mykyta Chernenky
+200367631
+CS 476 - Software Development
+uBooks
+*/
+
+//PROTOTYPE Design Patter - Part 2
+import { User } from "./user"; //import user class
+
+class Customer { //initiate customer class
+  constructor(customerID1: number, user1: User) {} //2 parameters: customerID, and User object
+  create(               //create Customer function
+    customerID: number,
+    login: string,
+    password: string,
+    country: string,
+    creditCard: string,
+    phoneNumber: string
+  ) {
+    var customer = new Customer(customerID, new User(login, password, country, creditCard, phoneNumber)); //created new Customer by cloning User
+  }
+}
---
+++ client/src/app/interfaces/user.ts
@@ -0,0 +1,32 @@
+/*
+Mykyta Chernenky
+200367631
+CS 476 - Software Development
+uBooks
+*/
+
+//PROTOTYPE Design Pattern Part 1 
+
+export class User { //User class that will be cloned
+  constructor(
+    private login: string,
+    private password: string,
+    private country: string,
+    private creditCard: string,
+    private phoneNumber: string
+  ) {}
+
+  Set( //function used to set values for the user
+    login1: string,
+    password1: string,
+    country1: string,
+    creditCard1: string,
+    phoneNumber1: string
+  ) {
+    this.login = login1;
+    this.password = password1;
+    this.country = country1;
+    this.creditCard = creditCard1;
+    this.phoneNumber = phoneNumber1;
+  }
+}
